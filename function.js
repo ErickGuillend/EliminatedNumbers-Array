38675310e744aca38b3f3343be366125f6454345
Erick Guillen <70987897+ErickGuillend@users.noreply.github.com>
2022-07-28 11:52:58 -0500

    refactor of the function remove number

--- function.js
+++ function.js
@@ -1,11 +1,10 @@
 
-
 const numbers = [2,3,4,5,6,32,246,98,30];
 
-    numbers.push (4);
+const removeNumber = (number) => {
+    return numbers.filter(n => n != number);
+} 
 
-    const result = numbers.filter(number => number != 4);
 
 
-
-    console.log(result);
+console.log(removeNumber(123));
