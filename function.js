a7484d92b3f0b804018a96273f4c4160fbd5e4c9
Erick Guillen <70987897+ErickGuillend@users.noreply.github.com>
2022-07-28 12:02:59 -0500

    Change line

--- function.js
+++ function.js
@@ -4,7 +4,7 @@ const numbers = [2,3,4,5,6,32,246,98,30];
 const removeNumber = (number) => {
     return numbers.filter(n => n != number);
 } 
+console.log(removeNumber(273));
 
 
 
-console.log(removeNumber(123));
